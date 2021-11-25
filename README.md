2ebf4fcc822784e58fe95a24abf14e50abdf1acf
Abdullah Çiçekli <abdullahcicekli@Abdullahs-MacBook-Pro.local>
2021-11-25 16:22:20 +0300

    Create | creating docker-compose for nginx&php

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sword-bank
-vulnerability server for testing
+
+This project is a vulnerability project. Created for testing of the system of **swordeye.io** 
+
---
+++ nginx(1.19.6)-php(5.6)/project/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>This is a test page.!</h1>
+    <div>
+        <p>Built for testing <b>nginx:1.19.16</b> and <b>php:5.6</b>  versions. Weaknesses of these products will be checked in our system.</p>
+        <p>Sword-Bank</p>
+    </div>
+</article>
